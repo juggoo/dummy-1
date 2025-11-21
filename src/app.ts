4dddd4844b27523635634500b129658ca3eff3f0
Yovan Juggoo <yovan.juggoo.fe@gmail.com>
2025-11-21 12:21:22 +0400

    Created express app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Application running!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening to port ${PORT}`);
+});
